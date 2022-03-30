d0e6c81c9f1b4d2ba771e57ce526c6e1da4a6d6d
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-30 15:23:35 +0200

    Ajout des méthodes CRUD sur le ts du dashboard

--- frontend/src/app/component/dashboard/dashboard.component.ts
+++ frontend/src/app/component/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { catchError, tap, throwError } from 'rxjs';
+import { Task } from 'src/app/model/task';
+import { CrudService } from 'src/app/service/crud.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  taskObj:Task=new Task()
+  taskArr:Task[]=[]
+
+  addTaskValue:string=''
+  editTaskValue:string=''
+
+  constructor(private crudService: CrudService) { }
 
   ngOnInit(): void {
+    this.taskObj = new Task();
+    this.taskArr = []
+    this.getAllTasks()
+  }
+
+
+  getAllTasks() {
+    this.crudService.getAllTasks().subscribe((res)=>{
+      this.taskArr = res
+    }, err=>{
+      alert("Unable to get list of tasks")
+    })
+  }
+
+  addTask(){
+    this.taskObj.task_name = this.addTaskValue
+    this.crudService.addTask(this.taskObj)
+      .pipe(
+        catchError(err => {
+          return throwError(() => {
+            new Error(err)
+            alert("Failed to add a task")
+          })
+        }),
+        tap(task => {
+          this.ngOnInit()
+          this.addTaskValue=''
+        }),
+      )
+      .subscribe()
+  }
+
+  editTask(){
+    this.taskObj.task_name = this.editTaskValue
+    this.crudService.editTask(this.taskObj)
+      .pipe(
+        catchError(err => {
+          return throwError(() => {
+            new Error(err)
+            alert("Failed to update task")
+          })
+        }),
+        tap(task => this.ngOnInit()),
+      )
+      .subscribe()
+  }
+
+  deleteTask(task:Task){
+    this.crudService.deleteTask(task)
+      .pipe(
+        catchError(err => {
+          return throwError(() => {
+            new Error(err)
+            alert("Failed to delete task")
+          })
+        }),
+        tap(task => this.ngOnInit()),
+      )
+      .subscribe()
+  }
+
+  call(task: Task){
+    this.taskObj = task
+    this.editTaskValue = task.task_name
   }
 
 }
