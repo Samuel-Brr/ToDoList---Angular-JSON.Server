38fefe83e518d559cacf07f25ba8ad098834a23a
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-30 13:29:15 +0200

    Html du dashboard, import de qq modules, parametrage du server json et du model task

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +15,10 @@ import { DashboardComponent } from './component/dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/service/crud.service.ts
+++ frontend/src/app/service/crud.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class CrudService {
 
-  constructor() { }
+  serviceURL: string;
+
+  constructor(private http: HttpClient) {
+    this.serviceURL = "http://localhost:3000/tasks"
+   }
 }
